026164e978c879490c1d9ed22137fd7d9b2e7cfb
First Repository <adilkhann724@gmail.com>
2024-06-18 13:32:03 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
 import './Appa.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import Alert from './components/alert';
+import { useState } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from 'react-router-dom';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  
+  const showAlert = (message, type) => {
+    setAlert({ msg: message, type: type });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.background = 'grey';
+      showAlert("Dark Mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
+    } else {
+      setMode('light');
+      document.body.style.background = 'white';
+      showAlert("Light Mode has been enabled", "primary");
+    }
+  };
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">Textutils</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
-              
-              
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
+      {/* <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} /> 
+        <Alert alert={alert} />
+
+        <div className='container my-3'>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm heading="Enter the text to Analyze" showAlert={showAlert} mode={mode} />} />
+          </Routes>
         </div>
-      </nav>
+        
+      </Router> */}
+
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} /> 
+        <Alert alert={alert} />
+
+        <div className='container my-3'>
+        <TextForm heading="Enter the text to Analyze" showAlert={showAlert} mode={mode} />
+        </div>  
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementsByClassName('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
